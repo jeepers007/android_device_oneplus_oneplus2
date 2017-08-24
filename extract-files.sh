dc9e5424500585e4746eac9f22463f83ebd0eb6b
jeepers007 <jstn.haggard@gmail.com>
2017-08-24 16:06:55 -0500

    op2 - uniform spacing, please...

--- extract-files.sh
+++ extract-files.sh
@@ -28,25 +28,25 @@ CM_ROOT="$MY_DIR"/../../..
 
 HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
+	echo "Unable to find helper script at $HELPER"
+	exit 1
 fi
 . "$HELPER"
 
 if [ $# -eq 0 ]; then
-  SRC=adb
+	SRC=adb
 else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
+	if [ $# -eq 1 ]; then
+	SRC=$1
+	else
+		echo "$0: bad number of arguments"
+		echo ""
+		echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+		echo ""
+		echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+		echo "the device using adb pull."
+		exit 1
+	fi
 fi
 
 # Initialize the helper
