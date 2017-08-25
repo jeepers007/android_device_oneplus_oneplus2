LineageOS/android_device_huawei_angler

Fixed thermal HAL, added skin temperature, thresholds.
Bug: 27424857
Change-Id: Ia28f9de4305523bc6149f3f1d12fad400df15370

device.mk
@@ -250,7 +250,7 @@ PRODUCT_PACKAGES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     power.angler \
-    hardware_properties.angler
+    thermal.angler
 
 PRODUCT_COPY_FILES += \
     device/huawei/angler/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \

jeepers007/android_device_oneplus_oneplus2

device.mk
@@ -225,7 +225,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-	power.oneplus2
+	power.oneplus2 \
+	thermal.oneplus2
 
 # Ramdisk
 PRODUCT_PACKAGES += \